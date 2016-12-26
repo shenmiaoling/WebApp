627e728aa225d77d7e1dd6a04739bbcf1c6d8f71
nodejs <shinchou@shinchou.local>
2016-12-26 17:53:17 +0800

    label

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,8 +1,19 @@
 // pages/user/user.js
+var app = getApp()
 Page({
-  data:{},
-  onLoad:function(options){
-    // 页面初始化 options为页面跳转所带来的参数
+  data:{
+    userInfo: {}
+  },
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+    })
   },
   onReady:function(){
     // 页面渲染完成
